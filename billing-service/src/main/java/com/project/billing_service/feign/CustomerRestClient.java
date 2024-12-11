bf0a809abbee11d0ad880f00d29ffe7295539184
TANTELY00 <tanteli.ia.engineer@gmail.com>
2024-12-11 16:55:42 +0300

    v9

--- billing-service/src/main/java/com/project/billing_service/feign/CustomerRestClient.java
+++ billing-service/src/main/java/com/project/billing_service/feign/CustomerRestClient.java
@@ -1,6 +1,7 @@
 package com.project.billing_service.feign;
 
 import com.project.billing_service.DTOs.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,15 @@ public interface CustomerRestClient {
     public PagedModel<Customer> getCustomer();
 
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerService" ,fallbackMethod = "getDefaultCustomer")
     public Customer getCustomerById(@PathVariable Long id);
+
+    default Customer getDefaultCustomer(Long id , Exception exception){
+        return Customer.builder()
+                .id(id)
+                .name("default Name")
+                .email("default email")
+                .build();
+    }
 
 }
